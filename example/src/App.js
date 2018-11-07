c4a667860258d62c47bc66c6d1f4c4966d62c26f
James Collins <jbccollins@gmail.com>
2018-11-06 20:38:55 -0500

    Initial lava lamp menu

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-lava-lamp-menu'
+import LavaLampMenu from 'react-lava-lamp-menu'
+
+const items = [
+	{name: 'People', id: 'people'},
+	{name: 'My Inbox', id: 'inbox'},
+	{name: 'Other Stuff', id: 'other'},
+	{name: 'Idk', id: 'idk'}
+]
 
 export default class App extends Component {
+  state = {
+  	selectedItemId: 'inbox'
+  }
+
+  handleSelectedItemChange = selectedItemId => {
+  	this.setState({selectedItemId})
+  }
+
   render () {
+  	const { selectedItemId } = this.state;
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className='app'>
+        <LavaLampMenu onSelectedItemChange={this.handleSelectedItemChange} items={items} selectedItemId={selectedItemId}/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,116 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export default class LavaLampMenu extends Component {
   static propTypes = {
-    text: PropTypes.string
+    items: PropTypes.array.isRequired,
+    selectedItemId: PropTypes.string,
+    menuPadding: PropTypes.number,
+    itemPadding: PropTypes.number,
+    onSelectedItemChange: PropTypes.func.isRequired,
+    selectedItemBackgroundColor: PropTypes.string
   }
+
+  state = {
+    sliderStyle: {},
+    mouseOverItemId: "",
+  }
+
+  componentDidMount() {
+    this.getSliderStyle();
+  }
+
+  componentDidUpdate() {
+    this.getSliderStyle()
+  }
+
+  handleItemMouseOver = id => {
+    //console.log('mouseover');
+    this.setState({
+      mouseOverItemId: id
+    })
+  }
+
+  handleMouseLeave = () => {
+    this.setState({
+      mouseOverItemId: ""
+    })
+  }
+
+  handleItemClick = id => {
+    console.log('click');
+    this.props.onSelectedItemChange(id);
+  }
+
+  getSliderStyle = () => {
+    const { selectedItemId, items, menuPadding, itemPadding } = this.props;
+    const { sliderStyle } = this.state;
+    const index = items.findIndex(({id}) => id === selectedItemId);
+    const selectedItemDiv = this[this.getItemId(selectedItemId)];
+    console.log(selectedItemDiv.getBoundingClientRect())
+    const {x, y, width, height} = selectedItemDiv.getBoundingClientRect();
+
+    const sliderMarginLeft = x - menuPadding;
+    const sliderWidth = width //+ itemPadding*2
+
+    if (sliderMarginLeft !== sliderStyle.marginLeft || sliderWidth !== sliderStyle.width) {
+      this.setState({
+        sliderStyle: { 
+          marginLeft: sliderMarginLeft,
+          width: sliderWidth
+        }
+      });
+    }
+  }
+
+  getItemId = id => `item-${id}`;
 
   render() {
     const {
-      text
+      items,
+      selectedItemId,
+      itemPadding,
+      sliderBackgroundColor,
+      selectedItemBackgroundColor
     } = this.props
+    const { sliderStyle, mouseOverItemId } = this.state;
+    console.log(this.refs);
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={styles.LavaLampMenu}>
+        <div className={styles.slider} style={{
+          backgroundColor: sliderBackgroundColor,
+          ...sliderStyle
+        }}></div>
+        {items.map(({id, name}) => {
+          const isSelected = id === selectedItemId;
+          const isMousedOver = id === mouseOverItemId;
+          return (
+            <div
+              ref={itemDiv => (this[this.getItemId(id)] = itemDiv)}
+              onClick={() => this.handleItemClick(id)}
+              onMouseOver={() => this.handleItemMouseOver(id)}
+              onMouseLeave={() => this.handleMouseLeave(id)}
+              key={id}
+              style={{
+                paddingLeft: itemPadding,
+                paddingRight: itemPadding,
+                background: isSelected || isMousedOver ? selectedItemBackgroundColor : ''
+              }}
+              className={`${styles.item} ${isSelected ? styles.selected : ''}`}>
+                <div className={styles.name}>{name}</div>
+            </div>
+          )
+        })}
       </div>
     )
   }
 }
+
+LavaLampMenu.defaultProps = {
+  menuPadding: 12,
+  itemPadding: 8,
+  sliderBackgroundColor: 'blue',
+  selectedItemBackgroundColor: 'rgba(0,0,255,0.1)'
+};
+
